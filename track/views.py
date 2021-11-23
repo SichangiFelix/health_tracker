c7789d045f9100f56ffc244b9dc8af7c0bdd516b
SichangiFelix <sichangifelix23@gmail.com>
2021-11-23 12:04:10 +0300

    implimented login action

--- track/views.py
+++ track/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -12,11 +15,27 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was successfuly created for ' + user)
+            return redirect('login')
 
     context = {'form' : form}
     return render(request, "screens/registration.html", context)
 
-def login (request):
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        Password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = Password)
+
+        if user is not None:
+            login(request, user)
+
+            return redirect('')
+
+    context = {}
     return render(request,"screens/login.html")
 
 def home(request):
