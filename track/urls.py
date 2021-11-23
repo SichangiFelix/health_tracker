62fe2e0d992829a2a2dc7a07dd715c1ca9025589
SichangiFelix <sichangifelix23@gmail.com>
2021-11-23 12:03:48 +0300

    Implimented login action

--- track/urls.py
+++ track/urls.py
@@ -4,8 +4,8 @@ from .import views
 
 urlpatterns=[
     path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
+    path('register', views.register, name='register'),
     path('land', views.land, name='land'),
     path('registration', views.registerPage, name='registration'),
-    path('login', views.login, name='login'),
+    path('login', views.loginPage, name='login'),
 ]
