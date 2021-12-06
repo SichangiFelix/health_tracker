974d33710fbdaca1847556804fe42915a04fdb69
SichangiFelix <sichangifelix23@gmail.com>
2021-12-06 19:04:28 +0300

    Updated the launch page to be the homepage

--- track/urls.py
+++ track/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('register', views.register, name='register'),
     path('land', views.land, name='land'),
     path('registration', views.registerPage, name='registration'),
-    path('login', views.loginPage, name='login'),
+    path('', views.loginPage, name='login'),
 ]
